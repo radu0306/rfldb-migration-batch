f5d398e757ffeb271e5ff21d4c8709cc845800e3
Radu <radu_a_03@yahoo.com>
2019-12-12 18:48:10 +0200

    merge

--- src/main/java/ro/raft/migrationBatch/config/BatchConfig.java
+++ src/main/java/ro/raft/migrationBatch/config/BatchConfig.java
@@ -1,7 +1,6 @@
 package ro.raft.migrationBatch.config;
 
 import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -12,132 +11,279 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.data.RepositoryItemWriter;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
-import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.LineMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import ro.astl.services.rfldbapi.country.dao.CountryDaoImpl;
-import ro.astl.services.rfldbapi.country.dao.CountryDaoInterface;
-import ro.astl.services.rfldbapi.country.dao.CountryRepository;
+import ro.astl.services.rfldbapi.club.dto.ClubIn;
 import ro.astl.services.rfldbapi.country.dto.CountryIn;
-import ro.astl.services.rfldbapi.country.manager.CountryManager;
+import ro.astl.services.rfldbapi.country.dto.RegionIn;
+import ro.astl.services.rfldbapi.league.dto.LeagueIn;
+import ro.raft.migrationBatch.mapper.ClubMapper;
 import ro.raft.migrationBatch.mapper.CountryMapper;
-import ro.raft.migrationBatch.processor.MigrationBatchProcessor;
-import ro.raft.migrationBatch.writer.MigrationBatchWriter;
+import ro.raft.migrationBatch.mapper.LeagueMapper;
+import ro.raft.migrationBatch.mapper.RegionMapper;
+import ro.raft.migrationBatch.processor.ClubProcessor;
+import ro.raft.migrationBatch.processor.CountryProcessor;
+import ro.raft.migrationBatch.processor.LeagueProcessor;
+import ro.raft.migrationBatch.processor.RegionProcessor;
+import ro.raft.migrationBatch.writer.ClubWriter;
+import ro.raft.migrationBatch.writer.LeagueWriter;
+import ro.raft.migrationBatch.writer.CountryWriter;
+import ro.raft.migrationBatch.writer.RegionWriter;
 
 @Configuration
 @EnableTransactionManagement
 @EnableBatchProcessing
 @EnableJpaRepositories("ro.astl")
-public class BatchConfig extends DefaultBatchConfigurer implements BatchConfigurer{
+public class BatchConfig extends DefaultBatchConfigurer implements BatchConfigurer {
 
 	@Autowired
-    private JobBuilderFactory jobBuilderFactory;
+	private JobBuilderFactory jobBuilderFactory;
 
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
-    
-    @Autowired
-    private EntityManagerFactory emf;
-    
-    @Autowired
-    private CountryMapper countryFieldMapper; 
-    
-    @Value("classPath:/input/countryList.csv")
-    private Resource inputResource;
-    
-    @Bean
-    public Job readCSVFileJob() {
-        return jobBuilderFactory
-                .get("readCountryCSVJob")
-                .incrementer(new RunIdIncrementer())
-                .start(step())
-                .build();
-    }
-    
-    @Bean
-    public Step step() {
-        return stepBuilderFactory
-                .get("step")
-                .<CountryIn, CountryIn>chunk(5)
-                .reader(reader())
-                .processor(processor())
-                .writer(writer())
-                .transactionManager(getBatchTransactionManager())
-                .build();
-    }
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
 
-    @Bean
-    public ItemProcessor<CountryIn, CountryIn> processor() {
-        return new MigrationBatchProcessor();
-    }
+	@Autowired
+	private EntityManagerFactory emf;
 
-    @Bean
-    public FlatFileItemReader<CountryIn> reader() {
-        FlatFileItemReader<CountryIn> itemReader = new FlatFileItemReader<CountryIn>();
-        itemReader.setLineMapper(lineMapper());
-        itemReader.setLinesToSkip(1);
-        itemReader.setResource(inputResource);
-        return itemReader;
-    	
-    }
-    
-    @Bean
-    public LineMapper<CountryIn> lineMapper() {
-        DefaultLineMapper<CountryIn> lineMapper = new DefaultLineMapper<CountryIn>();
-        DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+	@Autowired
+	private CountryMapper countryFieldMapper;
 
-        lineTokenizer.setDelimiter(",");
-        lineTokenizer.setNames(new String[]{"CountryName", "CountryShortName" });
-        lineTokenizer.setIncludedFields(new int[]{0, 1});
-        
-        lineMapper.setLineTokenizer(lineTokenizer);
-        lineMapper.setFieldSetMapper(countryFieldMapper);
+	@Autowired
+	private RegionMapper regionFieldMapper;
+	
+	@Autowired
+	private LeagueMapper leagueFieldMapper;
+	
+	@Autowired
+	private ClubMapper clubFieldMapper;
 
-        return lineMapper;
-    }
-    
-    @Bean
-    public ItemWriter<CountryIn> writer() {
-        return new MigrationBatchWriter();
-    }
-    
+	@Value("classPath:/input/countryList.csv")
+	private Resource countryResource;
 
-    
-    @Bean
-    public FieldSetMapper<CountryIn> getMapper(){
-    	return new CountryMapper();
-    }
-    
-    public PlatformTransactionManager getBatchTransactionManager() {
-    	JpaTransactionManager transactionManager = new JpaTransactionManager();
-    	transactionManager.setEntityManagerFactory(emf);
-    	return transactionManager;
+	@Value("classPath:/input/RegionList.csv")
+	private Resource regionResource;
+
+	@Value("classPath:/input/LeaguesList.csv")
+	private Resource leagueResource;
+	
+	@Value("classPath:/input/ClubList.csv")
+	private Resource clubResource;
+	
+	@Bean
+	public Job readCSVFileJob() {
+		return jobBuilderFactory.get("readCountryCSVJob").incrementer(new RunIdIncrementer()).start(countryStep())
+				.next(regionStep()).build();
+	}
+
+	@Bean
+	public Step countryStep() {
+		return stepBuilderFactory
+				.get("countryStep")
+				.<CountryIn, CountryIn>chunk(5)
+				.reader(countryReader())
+				.processor(countryProcessor())
+				.writer(countryWriter())
+				.transactionManager(getBatchTransactionManager())
+				.build();
+	}
+
+	@Bean
+	public Step regionStep() {
+		return stepBuilderFactory
+				.get("regionStep")
+				.<RegionIn, RegionIn>chunk(5)
+				.reader(regionReader())
+				.processor(regionProcessor())
+				.writer(regionWriter())
+				.transactionManager(getBatchTransactionManager())
+				.build();
+
+	}
+
+	@Bean
+	public Step leagueStep() {
+		return stepBuilderFactory
+				.get("leagueStep")
+				.<LeagueIn, LeagueIn>chunk(5)
+				.reader(leagueReader())
+				.processor(leagueProcessor())
+				.writer(leagueWriter())
+				.transactionManager(getBatchTransactionManager())
+				.build();
+	}
+	
+	@Bean
+	public Step clubStep() {
+		return stepBuilderFactory
+				.get("clubStep")
+				.<ClubIn, ClubIn>chunk(5)
+				.reader(clubReader())
+				.processor(clubProcessor())
+				.writer(clubWriter())
+				.transactionManager(getBatchTransactionManager())
+				.build();
+	}
+
+	@Bean
+	public FlatFileItemReader<CountryIn> countryReader() {
+		FlatFileItemReader<CountryIn> itemReader = new FlatFileItemReader<CountryIn>();
+		itemReader.setLineMapper(countryLineMapper());
+		itemReader.setLinesToSkip(1);
+		itemReader.setResource(countryResource);
+		return itemReader;
+
+	}
+
+	@Bean
+	public FlatFileItemReader<RegionIn> regionReader() {
+		FlatFileItemReader<RegionIn> itemReader = new FlatFileItemReader<RegionIn>();
+		itemReader.setLineMapper(regionLineMapper());
+		itemReader.setLinesToSkip(1);
+		itemReader.setResource(countryResource);
+		return itemReader;
+
+	}
+
+	private FlatFileItemReader<LeagueIn> leagueReader() {
+		FlatFileItemReader<LeagueIn> itemReader = new FlatFileItemReader<LeagueIn>();
+		itemReader.setLineMapper(leagueLineMapper());
+		itemReader.setLinesToSkip(1);
+		itemReader.setResource(leagueResource);
+		return itemReader;
+	}
+	
+	private FlatFileItemReader<ClubIn> clubReader() {
+		FlatFileItemReader<ClubIn> itemReader = new FlatFileItemReader<ClubIn>();
+		itemReader.setLineMapper(clubLineMapper());
+		itemReader.setLinesToSkip(1);
+		itemReader.setResource(clubResource);
+		return itemReader;
+	}
+
+	@Bean
+	public ItemProcessor<CountryIn, CountryIn> countryProcessor() {
+		return new CountryProcessor();
+	}
+
+	@Bean
+	public ItemProcessor<RegionIn, RegionIn> regionProcessor() {
+		return new RegionProcessor();
+	}
+	
+	@Bean
+	public ItemProcessor<LeagueIn, LeagueIn> leagueProcessor() {
+		return new LeagueProcessor();
+	}
+	
+	@Bean
+	public ItemProcessor<ClubIn, ClubIn> clubProcessor() {
+		return new ClubProcessor();
+	}
+
+	@Bean
+	public ItemWriter<CountryIn> countryWriter() {
+		return new CountryWriter();
+	}
+
+	@Bean
+	public ItemWriter<RegionIn> regionWriter() {
+		return new RegionWriter();
+	}
+	
+	@Bean
+	public ItemWriter<LeagueIn> leagueWriter() {
+		return new LeagueWriter();
+	}
+	
+	@Bean
+	public ItemWriter<ClubIn> clubWriter() {
+		return new ClubWriter();
+	}
+
+	@Bean
+	public FieldSetMapper<CountryIn> getMapper() {
+		return new CountryMapper();
+	}
+
+	@Bean
+	public LineMapper<CountryIn> countryLineMapper() {
+		DefaultLineMapper<CountryIn> lineMapper = new DefaultLineMapper<CountryIn>();
+		DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+		lineTokenizer.setDelimiter(",");
+		lineTokenizer.setNames(new String[] { "CountryName", "CountryShortName" });
+		lineTokenizer.setIncludedFields(new int[] { 0, 1 });
+
+		lineMapper.setLineTokenizer(lineTokenizer);
+		lineMapper.setFieldSetMapper(countryFieldMapper);
+
+		return lineMapper;
+	}
+
+	@Bean
+	public LineMapper<RegionIn> regionLineMapper() {
+		DefaultLineMapper<RegionIn> lineMapper = new DefaultLineMapper<RegionIn>();
+		DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+		lineTokenizer.setDelimiter(",");
+		lineTokenizer.setNames(new String[] { "RegionName", "RegionShortName" });
+		lineTokenizer.setIncludedFields(new int[] { 0, 1 });
+
+		lineMapper.setLineTokenizer(lineTokenizer);
+		lineMapper.setFieldSetMapper(regionFieldMapper);
+
+		return lineMapper;
+	}
+	
+	@Bean
+	public LineMapper<LeagueIn> leagueLineMapper() {
+		DefaultLineMapper<LeagueIn> lineMapper = new DefaultLineMapper<LeagueIn>();
+		DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+		lineTokenizer.setDelimiter(",");
+		lineTokenizer.setNames(new String[] { "LeagueName", "LeageNode", "RegionName", "RegionShortName" });
+		lineTokenizer.setIncludedFields(new int[] { 0, 1, 2, 3});
+
+		lineMapper.setLineTokenizer(lineTokenizer);
+		lineMapper.setFieldSetMapper(leagueFieldMapper);
+
+		return lineMapper;
+	}
+	
+	@Bean
+	public LineMapper<ClubIn> clubLineMapper() {
+		DefaultLineMapper<ClubIn> lineMapper = new DefaultLineMapper<ClubIn>();
+		DelimitedLineTokenizer lineTokenizer = new DelimitedLineTokenizer();
+
+		lineTokenizer.setDelimiter(",");
+		lineTokenizer.setNames(new String[] { "ClubName", "ClubNameShort", "CountryName", "LeagueName", "ClubFMId" });
+		lineTokenizer.setIncludedFields(new int[] { 0, 1, 2, 3, 4});
+
+		lineMapper.setLineTokenizer(lineTokenizer);
+		lineMapper.setFieldSetMapper(clubFieldMapper);
+
+		return lineMapper;
+	}
+
+	public PlatformTransactionManager getBatchTransactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
 
 	}
 }
